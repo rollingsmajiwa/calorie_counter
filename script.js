eb781c8f2cdb9b4f1af33a28db66e47d495933d9
rollingsmajiwa <rollingsmajiwa@gmail.com>
2026-06-14 15:05:07 +0300

    add index.html and script.js files

---
+++ script.js
@@ -0,0 +1,63 @@
+let foodItem = JSON.parse(localStorage.getItem("foodItem")) || [];
+
+
+const userForm = document.getElementById("userForm");
+const foodName = document.getElementById("food-name");
+const calorie = document.getElementById("calorie-input");
+const listItem = document.getElementById("listItem");
+const resetBtn = document.getElementById("reset-btn");
+const totalCalories = document.getElementById("totalCalories");
+
+
+
+function updateCalorie() {
+    const total = foodItem.reduce((sum, item) => sum + item.calories, 0)
+    totalCalories.textContent = `Total Calories = ${total}`;
+}
+
+function renderList() {
+    listItem.innerHTML = "";
+    foodItem.forEach((item, index) => {
+       let li = document.createElement("li");
+       li.className = "flex justify-center items-center py-2 text-gray-700"; 
+       //creating text containing food name and calorie number
+       let textSpan = document.createElement("span");
+       textSpan.textContent = `${item.name} (${item.calories}) Kcal`;
+       //creating remove button
+       let removeBtn = document.createElement("button");
+       removeBtn.textContent = "Delete";
+       removeBtn.className = "text-red-500 hover:text-red-700 font-bold text-sm";
+       
+       removeBtn.addEventListener("click", ()=> {
+        foodItem.splice(index, 1);
+        renderList();
+       });
+       li.appendChild(textSpan);
+       li.appendChild(removeBtn);
+
+       //add the finished created row on to the webpage listItem
+       listItem.appendChild(li)
+
+    });
+    updateCalorie();
+    
+   
+}
+userForm.addEventListener("submit", (event)=> {
+    event.preventDefault()
+    let name = foodName.value
+    let calories = parseInt(calorie.value, 10)
+
+    if(name && !isNaN(calories)) {
+        foodItem.push({name, calories});
+        localStorage.setItem("foodItem", JSON.stringify(foodItem));
+        renderList()
+        userForm.reset();
+    }
+});
+resetBtn.addEventListener("click", ()=> {
+    if(confirm("Are you sure you want to reset all?")) {
+        foodItem = [];
+        renderList();
+    }
+})
